bf32f4ba16b69fe89843382a082517414c9884b6
RSutton16 <150315871+RSutton16@users.noreply.github.com>
2024-01-29 23:29:22 -0600

    modified:   StatBaseball.py

--- StatBaseball.py
+++ StatBaseball.py
@@ -1,45 +1,44 @@
-class main:
+balls = 0
+strikes = 0
+outs = 0
+innings = 0
+
+def printStats():
+    print("\nCount " + str(balls) + "-" + str(strikes) + " Outs: " + str(outs) + " inning: " + str(innings) + "\n")
+
+def resetCount():
     balls = 0
     strikes = 0
-    outs = 0
-    innings = 0
-    pitchResult = None
 
-    def resetCount():
-        balls = 0
-        strikes = 0
+def inning():
+    if innings < 8:
+        innings += 1
+        outs = 0
 
-    def inning():
-        if innings < 8:
-            innings += 1
-            outs = 0
+def out():
+    if outs > 1:
+        outs = 0
+        inning()
+    else:
+        outs+=1
+    resetCount()
 
-    def out():
-        if outs > 1:
-            outs = 0
-            inning()
-        else:
-            outs+=1
-        resetCount()
+def moveRunner():
+    pass
 
-    def moveRunner():
-        pass
-    
-
-    def changeCount(pitchResult):
-        if pitchResult == "strike":
-            strikes += 1
-            if strikes >= 3:
-                out()
-        if pitchResult == "ball":
-            balls += 1
-            if balls >= 4:
-                moveRunner()
-        if pitchResult == "flyout":
+def changeCount(result):
+    if result == "strike":
+        strikes += 1
+        if strikes >= 3:
             out()
-        if pitchResult == "foulball":
-            if strike < 2:
-                strike += 1
+    if result == "ball":
+        balls += 1
+        if balls >= 4:
+            moveRunner()
+    if result == "flyout":
+        out()
+    if result == "foulball":
+        if strike < 2:
+            strike += 1
 
-    def printStats():
-        print("\nCount " + str(balls) + "-" + str(strikes) + " Outs: " + str(outs) + " inning: " + str(innings) + "\n")
+printStats()
